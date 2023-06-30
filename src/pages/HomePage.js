7a36d259636ca82a47d5af92afdb9ae662adb68d
Karan Doshi <karandoshi@karans-air.wifisec.seattleu.edu>
2023-06-29 22:30:18 -0700

    added css

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery, gql } from "@apollo/client";
 import { Link } from "react-router-dom";
+import { styled } from '@mui/material/styles';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const CountryList = gql`
   {
@@ -9,33 +17,77 @@ const CountryList = gql`
       capital
       currency
       emoji
-      continent{
+      continent {
         name
       }
     }
   }
 `;
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+}));
 function HomePage() {
-  const { data, error, loading } = useQuery(CountryList);
+  const { data, error, loading } = useQuery(CountryList); 
 
   return (
-    <div>
-      <h1>List of Countries</h1>
-      <Link to="/search">Search</Link>
+    <div className=" p-10">
+      <div className=" flex items-center justify-between mb-5">
+        <h1 className=" uppercase tracking-wider font-bold">
+          List of Countries
+        </h1>
+        <Link to="/search">
+          <button className=" bg-gray-200 p-2 rounded-md hover:bg-black hover:text-white">
+            Search
+          </button>
+        </Link>
+      </div>
+
       <div>
         {loading && <p>Loading...</p>}
         {error && <p>Error: {error.message}</p>}
+
         <div>
-          {data &&
-            data.countries.map((country, key) => {
-              return (
-                <div key={key}>
-                  <h1>{country.name}</h1>
-                  <h1 className=" text-green-600">{country.capital}</h1>
-                  <p className=" text-yellow-200">{country.continent.name}</p>
-                </div>
-              );
-            })}
+          {/* // <div key={key}>
+                //   <h1>{country.name}</h1>
+                //   <h1>{country.emoji}</h1>
+                //   <h1 className=" text-green-600">{country.capital}</h1>
+                //   <p className=" text-yellow-200">{country.continent.name}</p>
+                //   <h1>{country.currency}</h1>
+                // </div> */}
+          <TableContainer component={Paper}>
+            <Table stickyHeader sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead stickyHeader>
+                <TableRow>
+                  <StyledTableCell>COUNTRY</StyledTableCell>
+                  <StyledTableCell>FLAG</StyledTableCell>
+                  <StyledTableCell>CAPITAL</StyledTableCell>
+                  <StyledTableCell>CONTINENT</StyledTableCell>
+                  <StyledTableCell>CURRENCY</StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data &&
+                  data.countries.map((country, key) => {
+                    return (
+                      <TableRow key={key} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                        <TableCell  scope="row">{country.name}</TableCell>
+                        <TableCell>{country.emoji}</TableCell>
+                        <TableCell>{country.capital}</TableCell>
+                        <TableCell>{country.continent.name}</TableCell>
+                        <TableCell>{country.currency}</TableCell>
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </div>
       </div>
     </div>
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import { useLazyQuery, gql } from "@apollo/client";
 import { Link } from "react-router-dom";
-import {BiArrowBack} from 'react-icons/bi'
+import { BiArrowBack } from "react-icons/bi";
 
 const SearchACountry = gql`
-  query Country($code: ID!)
-  {
+  query Country($code: ID!) {
     country(code: $code) {
       name
       emoji
@@ -15,7 +14,6 @@ const SearchACountry = gql`
       }
       languages {
         name
-        native
       }
       currency
       phone
@@ -30,29 +28,67 @@ function SearchPage() {
       variables: { code: countrySearch.toUpperCase() },
     }
   );
-  
 
   const inputChange = (event) => {
     setCountrySearch(event.target.value);
   };
 
   return (
-    <div>
-
-    <div>
-      <Link to ="/"><BiArrowBack/></Link>
-      <p>Search for your country</p>
-    </div>
-      
-      <div>
-        <input onChange={inputChange} placeholder="Enter your country code" />
-        <button onClick={searchCountry}>Search</button>
+    <div className=" p-10">
+      <div className=" flex items-center gap-x-20 mb-5">
+        <Link to="/">
+          <BiArrowBack />
+        </Link>
+        <p className=" uppercase tracking-widest font-semibold">
+          Search for your country
+        </p>
       </div>
 
-      <div>
-        {data && (
-          <div>
-            <h1>{data.country.name}</h1>
+      <div className=" p-5 flex items-center justify-center gap-x-5">
+        <input
+          className=" p-2 outline-none rounded-md w-1/3 bg-gray-100 border border-black focus:bg-white"
+          onChange={inputChange}
+          placeholder="Enter your country code"
+        />
+        <button
+          className=" p-2 rounded-md bg-black text-white hover:bg-white hover:text-black hover:border hover:border-black"
+          onClick={searchCountry}
+        >
+          Search
+        </button>
+      </div>
+
+      <div className="flex items-center justify-center mt-10 p-5">
+        {data && data.country && (
+          <div className=" bg-gray-100 p-10 rounded-md">
+            <div className=" flex items-center justify-center gap-x-10">
+              <h1 className=" uppercase tracking-widest font-bold text-2xl">
+                {data.country.name}
+              </h1>
+              <h1 className=" ">{data.country.emoji}</h1>
+            </div>
+
+            <h1 className=" mt-5 tracking-widest text-sm font-light">
+              The capital of {data.country.name} is {data.country.capital}
+            </h1>
+            <h1 className=" mt-5 tracking-widest text-sm font-light">
+              {" "}
+              Its located in the continent of {data.country.continent.name}
+            </h1>
+
+            {/* {data.country.languages.map((language) => (
+        <div key={language.name}>
+          <h1>{language.name}</h1>
+          <h1>{language.native}</h1>
+        </div>
+      ))} */}
+
+            <h1 className=" mt-5 tracking-widest text-sm font-light">
+              The currency of the country is {data.country.currency}
+            </h1>
+            <h1 className=" mt-5 tracking-widest text-sm font-light">
+              Phone code: +{data.country.phone}
+            </h1>
           </div>
         )}
       </div>
